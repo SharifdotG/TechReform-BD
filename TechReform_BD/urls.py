eef04946bbe884cabb4058ac4579c00e1ed90655
Sharif Yousuf <sharifmdyousuf007@gmail.com>
2024-10-17 03:20:14 +0600

    First draft for showing ma'am

--- TechReform_BD/urls.py
+++ TechReform_BD/urls.py
@@ -16,7 +16,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Main_Site.views import *
 
 urlpatterns = [
+    path('', home, name='home'),
+    path('signin/', signin, name='signin'),
+    path('signup/', signup, name='signup'),
+    path('profile/', profile, name='profile'),
+    path('logout/', logout, name='logout'),
+    path('products/', products, name='products'),
+    path('productdetails/<int:pk>/', productdetails, name='productdetails'),
+    path('add-to-cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
+    path('remove-from-cart/<int:product_id>/', remove_from_cart, name='remove_from_cart'),
+    path('view-cart/', view_cart, name='view_cart'),
+    path('update-quantity/<int:product_id>/', update_quantity, name='update_quantity'),
+    path('proceed-to-checkout/', proceed_to_checkout, name='proceed_to_checkout'),
+    path('checkout/', checkout, name='checkout'),
+    path('pay_now/', pay_now, name='pay_now'),
+    path('orderconfirm/', orderconfirm, name='orderconfirm'),
     path('admin/', admin.site.urls),
 ]
